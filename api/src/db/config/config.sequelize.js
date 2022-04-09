5eb41b649af01b0e231a1edd549f558cfc5d749a
Gabriel <gayagocr@gmail.com>
2022-04-09 14:48:08 -0500

    config added

---
+++ api/src/db/config/config.sequelize.js
@@ -0,0 +1,84 @@
+require('dotenv').config()
+const {
+  DB_USER, DB_PASSWORD, DB_NAME,
+  DB_HOST, DB_DIALECT
+  } = process.env
+
+module.exports = {
+  development: {
+    username: `${DB_USER}`,
+    password: `${DB_PASSWORD}`,
+    database: `${DB_NAME}`,
+    host: `${DB_HOST}`,
+    dialect: `${DB_DIALECT}`,
+    ssl: true,
+    logging: false,
+    pool: {
+      min: 1,
+      max: 3,
+      idle: 10000,
+    },
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+  },
+  test: {
+    username: `${DB_USER}`,
+    password: `${DB_PASSWORD}`,
+    database: `${DB_NAME}`,
+    host: `${DB_HOST}`,
+    dialect: `${DB_DIALECT}`,
+    ssl: true,
+    logging: false,
+    pool: {
+      min: 1,
+      max: 3,
+      idle: 10000,
+    },
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+  },
+  production: {
+    username: `${DB_USER}`,
+    password: `${DB_PASSWORD}`,
+    database: `${DB_NAME}`,
+    host: `${DB_HOST}`,
+    dialect: `${DB_DIALECT}`,
+    ssl: true,
+    logging: false,
+    pool: {
+      min: 1,
+      max: 3,
+      idle: 10000,
+    },
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+  },
+}
+
