00cd257f67458d1783dc18c85f09b3b482bcd543
Gabriel <gayagocr@gmail.com>
2022-04-14 10:04:53 -0500

    filter created added

--- api/src/routes/types.route.js
+++ api/src/routes/types.route.js
@@ -7,9 +7,7 @@ type_route.get('/', async (req, res) => {
   const { filter, page } = req.query
   const offset = (parseInt(page || 1)-1)*12
   if (filter) {
-    const { count, rows } = filter === 'created'
-    ? await Pokemons.findAndCountAll({ where: { id: { [Op.gt]: 300 } }, include: Types })
-    : await Types.findAndCountAll({ where: { name: `${filter}` }, include: Pokemons, })
+    const { count, rows } = await Types.findAndCountAll({ where: { name: `${filter}` }, include: Pokemons, })
     res.status(rows.length === 0 ? 400 : 200)
     .json(rows.length === 0 ? { detail: 'Nothing be found' }
     :{
